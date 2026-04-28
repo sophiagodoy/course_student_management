1e83438b931d6d66377679a5e1ae4e9fd99bed5d
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2026-04-28 17:53:01 -0300

    Update course_student_management.pl

--- course_student_management.pl
+++ course_student_management.pl
@@ -1,159 +1,154 @@
-% Define quais cursos existem 
-% Estrutura: curso(codigo_curso, nome_curso)
-curso(1,informatica).
-curso(2,eletro_eletronica).
+% Banco de conhecimentos
+% Cursos - curso(id_curso, nome_do_curso)
+curso(1, informatica).
+curso(2, eletro_eletronica).
 
-% Define quais matérias existem 
-% Estrutura: materia(codigo_materia, nome_materia, aulas_por_semana)
-materia(1,tecnicas_de_programacao,8).
-materia(2,programacao_orientada_a_objetos,5).
-materia(3,estruturas_de_dados,4).
-materia(4,topicos_em_metodologias_de_programacao,3).
-materia(5,circuitos_eletricos,4).
-materia(6,eletronica_digital,5).
-materia(7,arquitetura_computadores,6).
-materia(8,microcontroladores,4).
+% Materias - materia(id_materia, nome_da_materia, aulas_por_semana)
+materia(1, tecnicas_de_programacao, 8).
+materia(2, programacao_orientada_a_objetos, 5).
+materia(3, estruturas_de_dados, 4).
+materia(4, topicos_em_metodologias_de_programacao, 3).
+materia(5, circuitos_eletricos, 4).
+materia(6, eletronica_digital, 5).
+materia(7, arquitetura_computadores, 6).
+materia(8, microcontroladores, 4).
 
-% Curriculo de cada curso 
-% Estrutura: curriculo(codigo_curso, lista_de_materias)
-curriculo(1,[1,2,3,4]). % O curso de código 1 tem as matérias 1,2,3,4
-curriculo(2,[5,6,7,8]).
+% Curriculo - curriculo(id_curso, lista_de_ids_das_materias)
+curriculo(1, [1,2,3,4]). % O curso 1 tem as matérias 1,2,3,4
+curriculo(2, [5,6,7,8]).
 
-% Define os alunos
-% Estrutura: aluno(ra, nome)
-aluno(12808,jose).
-aluno(12080,marcos).
-aluno(12909,joao).
-aluno(12090,ana).
+% Alunos - aluno(ra_do_aluno, nome_do_aluno)
+aluno(12808, jose).
+aluno(12080, marcos).
+aluno(12909, joao).
+aluno(12090, ana).
 
-% Define o que o aluno cursa 
-% Estrutura: cursa(ra, codigo_curso)
-cursa(12909,1).
-cursa(12080,2).
-cursa(12090,2).
+% Quem cursa o que - cursa(ra_do_aluno, id_curso)
+cursa(12909, 1).
+cursa(12080, 2).
+cursa(12090, 2).
 
-% Histórico escolar de cada aluno 
-% Estrutura: item(cod_materia, semestre, ano, nota, frequencia)
-historico(12808,
-[
-item(1,1,2012,3.0,0.77),
-item(1,2,2013,6.5,0.90),
-item(5,1,2014,8.0,0.80)
+% Historico - historico(ra_do_aluno, lista_de_itens)
+historico(12808, [
+    % Item - representa uma disciplina que o aluno cursou
+    % item(id_materia, semestre, ano, nota, frequencia)
+    item(1,1,2012,3.0,0.77),
+    item(1,2,2013,6.5,0.90),
+    item(5,1,2014,8.0,0.80)
 ]).
 
-historico(12909,
-[
-item(1,1,2012,7.0,0.80),
-item(2,2,2013,8.5,0.80),
-item(3,1,2014,5.0,0.75)
+historico(12909, [
+    item(1,1,2012,7.0,0.80),
+    item(2,2,2013,8.5,0.80),
+    item(3,1,2014,5.0,0.75)
 ]).
 
-historico(12080,
-[
-item(5,1,2012,6.0,0.70),
-item(5,2,2013,7.5,0.90),
-item(6,1,2014,5.0,0.90)
+historico(12080, [
+    item(5,1,2012,6.0,0.70),
+    item(5,2,2013,7.5,0.90),
+    item(6,1,2014,5.0,0.90)
 ]).
 
-historico(12090,
-[
-item(7,1,2012,6.0,0.75),
-item(8,2,2014,8.0,0.89)
+historico(12090, [
+    item(7,1,2012,6.0,0.75),
+    item(8,2,2014,8.0,0.89)
 ]).
 
-% Criando a regra aprovado que verifica se o aluno foi aprovado na materia 
-% Aprovado se: nota >= 6 e frequencia >= 0.75
-aprovado(RA,CodigoMateria) :-
-	
-    % Busca o histórico do aluno 
-    historico(RA,ListaHistorico),
-	
-    % Verifica se algo está dentro de uma lista (member) 
-    member(
-        item(CodigoMateria,_,_,Nota,Frequencia),
-        ListaHistorico
-    ),
-	
-    Nota >= 6, % Verifica se a nota do aluno é maior ou igual a 6 
-    Frequencia >= 0.75. % Verifica se a frequência é pelo menos 75%
+% CONCLUIU 
+% Verifica se um elemento está contido em uma lista
+pertence(X, [X|_]). % Se o primeiro da lista for igual ao X 
+pertence(X, [_|R]) :- pertence(X, R). % Se não for o primeiro, ignora ele e procura no resto
 
-% Cria a regra concluiu que verifica se o aluno concluiu o curso 
-% Ele precisa estar aprovado em TODAS as materias do curriculo
-concluiu(RA,CodigoCurso) :-
-	
-    % Pega as matérias obrigatórias do curso 
-    curriculo(CodigoCurso,ListaMaterias),
+% Verifica se o aluno foi aprovado na matéria 
+aprovado(RA, CodMateria) :-
+    historico(RA, ListaHistorico), 
+    
+    % Procura no histórico um item dessa matéria
+    pertence(item(CodMateria, _, _, Nota, Frequencia), ListaHistorico), 
+    Nota >= 5.0,
+    Frequencia >= 0.75. 
 
-    forall(
-        member(CodigoMateria,ListaMaterias),
-        aprovado(RA,CodigoMateria)
-    ).
+% Verifica se o aluno concluiu o curso 
+todas_aprovadas(_, []). % Se o aluno não tem amis matérias ele já passou em todas
 
-% Cria a regra falta que lista quais materias ainda faltam para concluir o curso 
-falta(RA,CodigoCurso,ListaMateriasFaltantes) :-
-	
-    % Pega as matérias obrigatórias do curso 
-    curriculo(CodigoCurso,ListaMaterias),
+% O aluno passou na primeira matéria e também passou em todas as outras
+todas_aprovadas(RA, [M|R]) :-
+    aprovado(RA, M),
+    todas_aprovadas(RA, R).
 
-    findall(
-        NomeMateria,
-        
-        (
-            member(CodigoMateria,ListaMaterias),
-            \+ aprovado(RA,CodigoMateria),
-            materia(CodigoMateria,NomeMateria,_)
-        ),
-        
-        ListaMateriasFaltantes
-    ).
+concluiu(RA, CodigoCurso) :-
+    cursa(RA, CodigoCurso), % O aluno faz esse curso?
+    curriculo(CodigoCurso, Materias), % Quais matérias esse curso tem?
+    todas_aprovadas(RA, Materias). % O aluno passou em todas essas matérias?
 
-% Cria a regra extra que mostra quais materias o aluno fez, mas que não pertencem ao curso dele 
-extra(RA,CodigoCurso,ListaMateriasExtras) :-
-	
-    % Pega o histórico escolar do aluno 
-    historico(RA,ListaHistorico),
-	
-    % Pega as matérias obrigatorias do curso 
-    curriculo(CodigoCurso,ListaMateriasCurso),
+% FALTAS 
+% Verifica quais matérias falta para o aluno terminar o curso 
+pega_faltas(_, [], []). % Se não tem mais matérias, não falta nada
 
-    findall(
-        NomeMateria,
-        (
+% Se o aluno NÃO passou na matéria, coloca ela na lista de faltas
+pega_faltas(RA, [H|T], [Nome|Resto]) :-
+    \+ aprovado(RA, H), % O aluno não passou nessa materia 
+    materia(H, Nome, _), % Pega o nome da materia 
+    pega_faltas(RA, T, Resto). % Adiciona o nome na lista 
 
-            member(
-                item(CodigoMateria,_,_,_,_),
-                ListaHistorico
-            ),
-            
-            \+ member(CodigoMateria,ListaMateriasCurso),
-            materia(CodigoMateria,NomeMateria,_)
-        ),
-        
-        ListaMateriasExtras
-    ).
+% Se o aluno JÁ passou nessa matéria, ignora ela
+pega_faltas(RA, [H|T], Resto) :-
+    aprovado(RA, H), % O aluno passou nessa matéria?
+    pega_faltas(RA, T, Resto). % Verifica se ele passou no resto da lista 
 
-% Cria a regra jafoi que mostra qual porcentual do curso o aluno já concluiu
-jafoi(RA,CodigoCurso,PercentualConcluido) :-
-	
-    % Pega as matérias obrigatorias do curso 
-    curriculo(CodigoCurso,ListaMateriasCurso),
-	
-    % Conta quantas materias obrigatorias o curso tem, ou seja, quantos elementos tem dentro da lista 
-    length(ListaMateriasCurso,TotalMaterias),
+falta(RA, CC, OQUE) :-
+    cursa(RA, CC), % O aluno faz esse curso?
+    curriculo(CC, ListaMaterias), % Quais matérias esse curso tem?”
+    pega_faltas(RA, ListaMaterias, OQUE). % Dessas matérias, quais o aluno não passou?
 
-    findall(
-        CodigoMateria,
-        (
-            member(CodigoMateria,ListaMateriasCurso),
-            aprovado(RA,CodigoMateria)
-        ),
+% EXTRA
+% Quais matérias o aluno fez que NÃO pertencem ao curso?
+pega_extra(_, _, [], []). % Se não tem mais histórico, acabou 
 
-        ListaMateriasFeitas
-    ),
-	
-    % Conta quantas materias o aluno ja concluiu 
-    length(ListaMateriasFeitas,QuantidadeFeitas),
-	
-    % Calcula a pocentagem do curso do aluno
-    PercentualConcluido is
-        (QuantidadeFeitas / TotalMaterias) * 100.
+% Quando a matéria é extra...
+pega_extra(RA, Curriculo, [item(CM,_,_,_,_)|T], [Nome|Resto]) :-
+    aprovado(RA, CM), % Aluno passou 
+    \+ pertence(CM, Curriculo), % Não está no curso do aluno 
+    materia(CM, Nome, _),
+    pega_extra(RA, Curriculo, T, Resto).
+
+% Quando a matéria não é extra...
+pega_extra(RA, Curriculo, [_|T], Resto) :-
+    pega_extra(RA, Curriculo, T, Resto).
+
+extra(RA, CC, QUAIS) :-
+    cursa(RA, CC),
+    curriculo(CC, ListaMaterias),
+    historico(RA, Historico),
+    pega_extra(RA, ListaMaterias, Historico, QUAIS).
+
+% CONTAGEM
+% Conta quantas matérias o aluno passou e quantas matérias existem no curso
+
+% Se não tem mais matérias, o total é 0
+conta_aprovadas(_, [], 0).
+
+% Se passou na matéria, soma + 1
+conta_aprovadas(RA, [H|T], N) :-
+    aprovado(RA, H),
+    conta_aprovadas(RA, T, N1),
+    N is N1 + 1.
+
+% Se não passou, não soma nada
+conta_aprovadas(RA, [H|T], N) :-
+    \+ aprovado(RA, H),
+    conta_aprovadas(RA, T, N).
+
+conta_elementos([], 0).
+
+conta_elementos([_|T], N) :-
+    conta_elementos(T, N1),
+    N is N1 + 1.
+
+% PERCENTUAL
+% Quantos % do curso o aluno já completou?
+jafoi(RA, CC, QUANTO) :-
+    curriculo(CC, ListaMaterias),
+    conta_aprovadas(RA, ListaMaterias, Feitas),
+    conta_elementos(ListaMaterias, Total),
+    QUANTO is (Feitas / Total) * 100.
